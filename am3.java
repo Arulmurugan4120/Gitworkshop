2ff151359c6587a6a229b0ee9af82ce6a211a820
Christober <christobersg@gmail.com>
2023-11-11 07:42:40 +0530

    Modified some changes

--- am3.java
+++ am3.java
@@ -1,8 +1,9 @@
 class am3 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I am Arulmurugan N");
+        System.out.print("I am Arulmurugan N");
         System.out.println("It's nice session here");
         System.out.println("Updated version");
+
     }
 }
